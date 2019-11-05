08cf797c4aac2fc8db1ff73cad090411146ba393
bluebat1 <3272762667@qq.com>
2019-11-05 14:24:38 +0800

    index.html->alter

--- js/main.js
+++ js/main.js
@@ -55,179 +55,183 @@ document.getElementById("window2_close").onclick = function() {
 	window.onscroll = scrollFunc_back;
 }
 
-document.getElementById("continue_buttton").onclick = function() {
-	document.documentElement.scrollTop = window.innerWidth * 0.3;
-}
 window.onload = function() {
-	check_screen();
-	let browser = function() {
-		let u = navigator.userAgent,
-			app = navigator.appVersion;
-
-		return { //移动终端浏览器版本信息 
-			trident: u.indexOf("Trident") > -1, //IE内核
-			presto: u.indexOf("Presto") > -1, //opera内核
-			isWebKit: u.indexOf("AppleWebKit") > -1, //苹果、谷歌内核
-			gecko: u.indexOf("Gecko") > -1 && u.indexOf("KHTML") == -1, //火狐内核
-			isMobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
-			isIos: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
-			isAndroid: u.indexOf("Android") > -1 || u.indexOf("Linux") > -1, //android终端或者uc浏览器
-			iPhone: u.indexOf("iPhone") > -1, //是否为iPhone或者QQHD浏览器
-			iPad: u.indexOf("iPad") > -1, //是否iPad
-			webApp: u.indexOf("Safari") == -1, //是否web应该程序，没有头部与底部
-			isWeChat: u.toLowerCase().match(/MicroMessenger/i) == 'micromessenger'
-		};
-	}();
+//	check_screen();
+//	let browser = function() {
+//		let u = navigator.userAgent,
+//			app = navigator.appVersion;
+//
+//		return { //移动终端浏览器版本信息 
+//			trident: u.indexOf("Trident") > -1, //IE内核
+//			presto: u.indexOf("Presto") > -1, //opera内核
+//			isWebKit: u.indexOf("AppleWebKit") > -1, //苹果、谷歌内核
+//			gecko: u.indexOf("Gecko") > -1 && u.indexOf("KHTML") == -1, //火狐内核
+//			isMobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+//			isIos: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+//			isAndroid: u.indexOf("Android") > -1 || u.indexOf("Linux") > -1, //android终端或者uc浏览器
+//			iPhone: u.indexOf("iPhone") > -1, //是否为iPhone或者QQHD浏览器
+//			iPad: u.indexOf("iPad") > -1, //是否iPad
+//			webApp: u.indexOf("Safari") == -1, //是否web应该程序，没有头部与底部
+//			isWeChat: u.toLowerCase().match(/MicroMessenger/i) == 'micromessenger'
+//		};
+//	}();
 
 	function webgl_view() {
 		let stats, controls;
 		let camera, scene, renderer, light;
-		let clock = new THREE.Clock();
-		let mixer;
 		let container = document.getElementById("exhibition");
 
 		let vw = container.offsetWidth,
 			vh = container.offsetHeight;
-		console.log("vw:" + vw + "vh:" + vh);
-		let start = Date.now(),
-			fov = 30;
+		let start = Date.now();
+		let objs={};
+		
+		let canvas = document.createElement("canvas");
+		canvas.width = vw;
+		canvas.height = vh;
+		container.appendChild(canvas);
+		
+		// Load the 3D engine
+		var engine = new BABYLON.Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true});
+		// CreateScene function that creates and return the scene
+		var createScene = function(){
+		    // Create a basic BJS Scene object
+		    scene = new BABYLON.Scene(engine);
+		    // Create a FreeCamera, and set its position to {x: 0, y: 5, z: -10}
+		    //VirtualJoysticksCamera UniversalCamera
+		    camera = new BABYLON.UniversalCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
+		    // Target the camera to scene origin
+		    camera.setTarget(BABYLON.Vector3.Zero());
+		    // Attach the camera to the canvas
+		    camera.attachControl(canvas, true);
+		    //camera.maxZ = 100;//视野
+			//替换 控制 键
+			camera.keysUp = [87];
+		    camera.keysDown = [83];
+		    camera.keysLeft = [65];
+		    camera.keysRight = [68];
+		    
+		    camera.rotation.x = 0.1; //
+		    
+		    //Enable Collisions
+//		    scene.enablePhysics();
+//		    scene.gravity = new BABYLON.Vector3(0, -0.9, 0);
+			scene.collisionsEnabled = true;
+			
+			camera.ellipsoid = new BABYLON.Vector3(1, 1, 1);
+			camera.collisionRadius = new BABYLON.Vector3(0.5, 0.5, 0.5)
+			camera.checkCollisions = true;
+//			camera.applyGravity = true;
+//		    camera.addChild
+		    
+		    
+		    // Create a basic light, aiming 0, 1, 0 - meaning, to the sky
+		    var l_HemisphericLight = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+		    var l_PointLight = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(1, 10, -10), scene);
+		    // Create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation
+//		    var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene, false, BABYLON.Mesh.FRONTSIDE);
+//		    // Move the sphere upward 1/2 of its height
+//		    sphere.position.y = 2;
+//		    sphere.checkCollisions = true;
+//		    sphere.applyGravity = true;
+//		    sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
+		    
+		    // Water material
+		    var waterMaterial = new BABYLON.WaterMaterial("waterMaterial", scene, new BABYLON.Vector2(512, 512));
+		    waterMaterial.bumpTexture = new BABYLON.Texture("img/texture/waterbump.png", scene);
+		    waterMaterial.windForce = -10;
+		    waterMaterial.waveHeight = 0.05;
+		    waterMaterial.bumpHeight = 0.1;
+		    waterMaterial.waveLength = 0.1;
+		    waterMaterial.waveSpeed = 50.0;
+		    waterMaterial.colorBlendFactor = 0;
+		    waterMaterial.windDirection = new BABYLON.Vector2(1, 1);
+		    waterMaterial.colorBlendFactor = 0;
+		    waterMaterial.opacity = 0.5;
+		    
+		    // Water mesh
+		    var waterMesh = BABYLON.Mesh.CreateGround("waterMesh", 1200, 1200, 32, scene, false);
+		    waterMesh.material = waterMaterial;
+		    
+		    // Create a built-in "ground" shape; its constructor takes 6 params : name, width, height, subdivision, scene, updatable
+		    var ground = BABYLON.Mesh.CreateGround('ground1', 1200, 1200, 32, scene, false);
+		    ground.material = new  BABYLON.StandardMaterial("texture1", scene);
+			
+		    ground.material.diffuseTexture = new BABYLON.Texture("img/20129917170910519344.jpg", scene);
+//			ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
+		    ground.checkCollisions = true;
+		    
+		    
+		    
+		    
+		    var ffBox = new BABYLON.Mesh.CreateBox("box", 1000.0, scene, true, BABYLON.Mesh.DOUBLESIDE);
+		    ffBox.material = new  BABYLON.StandardMaterial("texture2", scene);
+		    ffBox.material.reflectionTexture = new BABYLON.CubeTexture("img/skybox/skybox", scene);
+		    ffBox.material.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+		    ffBox.material.disableLighting = true;
+		    ffBox.targetPosition = camera.position;
+		    ffBox.rotation.y = Math.PI;
+		    
+		    
+		    BABYLON.SceneLoader.LoadAssetContainer("./model/", "XiaoQiao.babylon", scene, function (container) {
+			    var meshes = container.meshes;
+			    var materials = container.materials;
+			    //...
+			    console.log(container);
+				for (var i = 0; i < meshes.length; i++) {
+					meshes[i].scaling = new BABYLON.Vector3(0.005, 0.005, 0.005);
+					meshes[i].position = new BABYLON.Vector3(0.0, -2.0, 0.0);
+					waterMaterial.addToRenderList(meshes[i]);
+				}
+			    // Adds all elements to the scene
+			    container.addAllToScene();
+			});
+		    
+		    
+		    
+		    
+		    objs.ffBox = ffBox;
+		    objs.camera = camera;
+		    
+		    waterMaterial.addToRenderList(ground);
+		    waterMaterial.addToRenderList(ffBox);
+		    
+		    
+		    
+		    console.log(objs.camera);
+		    // Return the created scene
+//		    return scene;
+		}
+		
+		createScene();
 
-		let timeUniform = {
-			iGlobalTime: {
-				type: 'f',
-				value: 0.1
-			},
-			iResolution: {
-				type: 'v2',
-				value: new THREE.Vector2()
+	
+	// run the render loop
+		engine.runRenderLoop(function(){
+			objs.ffBox.position = objs.camera.globalPosition;
+			if(objs.camera.position.x>1000){
+				objs.camera.position.x = 1000;
+			}else if(objs.camera.position.x<-1000){
+				objs.camera.position.x = -1000;
+			}else if(objs.camera.position.z>1000){
+				objs.camera.position.z = 1000;
+			}else if(objs.camera.position.z<-1000){
+				objs.camera.position.z = -1000;
+			}else if(objs.camera.position.y>20){
+				objs.camera.position.y = 20;
 			}
-		};
-		timeUniform.iResolution.value.x = vw;
-		timeUniform.iResolution.value.y = vh;
-
-		init();
-		animate();
-
-		function init() {
-			camera = new THREE.PerspectiveCamera(45, vw / vh, 1, 10000);
-			camera.position.set(0, 100, 300);
-
-			scene = new THREE.Scene();
-			scene.background = new THREE.Color(0xa0a0a0);
-			scene.fog = new THREE.Fog(0xa0a0a0, 200, 1000);
-			light = new THREE.HemisphereLight(0xffffff, 0x444444);
-			light.position.set(0, 200, 0);
-			scene.add(light);
-			light = new THREE.DirectionalLight(0xffffff);
-			light.position.set(0, 200, 100);
-			light.castShadow = true;
-			light.shadow.camera.top = 180;
-			light.shadow.camera.bottom = -100;
-			light.shadow.camera.left = -120;
-			light.shadow.camera.right = 120;
-			scene.add(light);
-			// scene.add( new CameraHelper( light.shadow.camera ) );
-
-			var axis = new THREE.AxesHelper(10);
-			scene.add(axis);
-			var s_geometry = new THREE.SphereGeometry(200, 32, 32);
-			var b_material = new THREE.MeshBasicMaterial({
-				color: 0xffff00,
-				depthWrite: true,
-			});
-
-			var sp_material = new THREE.ShaderMaterial({
-				uniforms: timeUniform,
-				vertexShader: document.getElementById('vertex-shader').textContent,
-				fragmentShader: document.getElementById('fragment-shader').textContent,
-				depthWrite: false,
-				depthTest: false
-			});
-			var water = new THREE.Mesh(new THREE.PlaneBufferGeometry(10, 10), sp_material);
-			//				water.rotation.x = - Math.PI / 2;
-			water.position.set(0, -100, 0);
-			scene.add(water);
-
-			let sphere = new THREE.Mesh(s_geometry, b_material);
-			//				scene.add( sphere );
-
-			// ground
-			let mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(2000, 2000), new THREE.MeshPhongMaterial({
-				color: 0x999999,
-				depthWrite: false
-			}));
-			mesh.rotation.x = -Math.PI / 2;
-			mesh.receiveShadow = true;
-			//				scene.add( mesh );
-			let grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000);
-			grid.material.opacity = 0.2;
-			grid.material.transparent = true;
-			//				scene.add( grid );
-			// model
-			//				let loader = new THREE.FBXLoader();
-			//				loader.load('model/233.fbx', function ( object ) {
-			////					mixer = new THREE.AnimationMixer( object );
-			////					let action = mixer.clipAction( object.animations[ 0 ] );
-			////					action.play();
-			////					object.traverse( function ( child ) {
-			////						if ( child.isMesh ) {
-			////							child.castShadow = true;
-			////							child.receiveShadow = true;
-			////						}
-			////					} );
-			//					object.scale.x=0.05;
-			//					object.scale.y=0.05;
-			//					object.scale.z=0.05;
-			////					
-			////					scene.add( object );
-			////					console.log(object);
-			//				} );
-
-			renderer = new THREE.WebGLRenderer({
-				antialias: true
-			});
-			renderer.setPixelRatio(window.devicePixelRatio);
-			renderer.setSize(vw, vh);
-			renderer.shadowMap.enabled = true;
-			container.appendChild(renderer.domElement);
-
-			controls = new THREE.OrbitControls(camera, renderer.domElement);
-			controls.target.set(0, 0, 0);
-			controls.update();
-			window.addEventListener('resize', onWindowResize, false);
-			// stats
-			//				stats = new Stats();
-			//				container.appendChild( stats.dom );
-
-			camera.lookAt(scene.position);
-		}
-
-		function onWindowResize() {
-			vw = container.offsetWidth;
-			vh = container.offsetHeight;
-			camera.aspect = vw / vh;
-			camera.updateProjectionMatrix();
-			renderer.setSize(vw, vh);
-			//console.log(vw+"-"+vh);
-		}
-		//
-		var delta;
-
-		function animate() {
-			requestAnimationFrame(animate);
-			delta = clock.getDelta();
-			if(mixer) mixer.update(delta);
-			renderer.render(scene, camera);
-
-			timeUniform.iGlobalTime.value -= delta * 1.5;
-
-			//				stats.update();
-		}
+			
+		    scene.render();
+		});
+		window.addEventListener('resize', function(){
+		    engine.resize();
+		});
 	}
-	if(browser.isMobile || browser.isIos || browser.iPhone || browser.iPad) {
-		let container = document.getElementById("exhibition");
-		container.innerHTML = "<center><h2>你的設備無法完美支持<h2></center>";
-	} else {
-		webgl_view();
-	}
+//	if(browser.isMobile || browser.isIos || browser.iPhone || browser.iPad) {
+//		let container = document.getElementById("exhibition");
+//		container.innerHTML = "<center><h2>你的設備無法完美支持<h2></center>";
+//	} else {
+//		
+//	}
+	webgl_view();
 }
